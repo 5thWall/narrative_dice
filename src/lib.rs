2a75ed21f84a84d2cbdf9e414e1b2728cbac5674
Andrew Arminio <andrew.arminio@optum.com>
2019-02-08 13:44:29 -0700

    Add Pool struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,36 @@
+struct Pool {
+    boost: u8,
+    hazard: u8,
+    skill: u8,
+    challenge: u8,
+    proficiency: u8,
+    setback: u8,
+}
+
+impl Pool {
+    fn new(boost: u8, hazard: u8, skill: u8, challenge: u8, proficiency: u8, setback: u8) -> Self {
+        Self {
+            boost,
+            hazard,
+            skill,
+            challenge,
+            proficency,
+            setback,
+        }
+    }
+
+    fn blank() -> Self {
+        Self {
+            boost: 0,
+            hazard: 0,
+            skill: 0,
+            challenge: 0,
+            proficency: 0,
+            setback: 0,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
